d9b6722ef6bb9e0b2ed5dda9a960abac3d90bdaf
Brymix Dev <dev@brymix.com>
2026-01-29 19:07:56 -0800

    Revert to direct Express app export for Vercel

--- brymix-dashboard/api/index.js
+++ brymix-dashboard/api/index.js
@@ -1,6 +1,4 @@
 const app = require('../server/index.js');
 
-// Export the Express app as a Vercel serverless function
-module.exports = (req, res) => {
-  return app(req, res);
-};
+// Export the Express app directly for Vercel
+module.exports = app;
